dfc9163fd0d2b60172f2f1cc9400e730c0b0e3e0
Luke <luke.oreilly@gmail.com>
2014-04-07 20:42:05 +1000

    Unit test for echo service

--- front/ngPlay.front.SPA/test/spec/services/echoservice.js
+++ front/ngPlay.front.SPA/test/spec/services/echoservice.js
@@ -6,13 +6,47 @@ describe('Service: EchoService', function () {
   beforeEach(module('ngPlay'));
 
   // instantiate service
-  var echoService;
-  beforeEach(inject(function (_echoService_) {
-    echoService = _echoService_;
+  var echoSvc, $httpBackend;
+  beforeEach(inject(function($injector) {
+    $httpBackend = $injector.get('$httpBackend');
+    echoSvc = $injector.get('EchoService');
   }));
 
-  //it('should do something', function () {
-  //  expect(!!echoService).toBe(true);
-  //});
+  it('should resolve the promise when successful HTTP GET', function () {
+
+    var sampleData = JSON.stringify({echo: 'echo response'});
+
+    $httpBackend.expectGET('http://localhost:51995/api/echo?value=hello').
+      respond(200, sampleData);
+
+    var promiseResolved = false;
+    var returnedData = '';
+    echoSvc.pingServer('hello').
+      then(function(data) {
+        promiseResolved = true;
+        returnedData = data;
+      });
+
+    $httpBackend.flush();
+
+    expect(promiseResolved).toBe(true);
+    expect(JSON.stringify(returnedData)).toBe(sampleData);
+  });
+
+  it('should reject the promise when failed HTTP GET', function () {
+
+    $httpBackend.expectGET('http://localhost:51995/api/echo?value=hello').
+      respond(500, JSON.stringify({echo: 'echo response'}));
+
+    var promiseRejected = false;
+    echoSvc.pingServer('hello').
+      then(undefined, function() {
+        promiseRejected = true;
+      });
+
+    $httpBackend.flush();
+
+    expect(promiseRejected).toBe(true);
+  });
 
 });
