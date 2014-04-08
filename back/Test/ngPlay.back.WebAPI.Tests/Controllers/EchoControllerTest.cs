5886fcee584b362de60c7327993dc61e86e146be
Luke <luke.oreilly@gmail.com>
2014-04-08 20:09:57 +1000

    Added simple unit test for echo controller

---
+++ back/Test/ngPlay.back.WebAPI.Tests/Controllers/EchoControllerTest.cs
@@ -0,0 +1,43 @@
+﻿using ngPlay.back.WebAPI.Controllers;
+using NUnit.Framework;
+using System.IO;
+using System.Web;
+
+///////////////////////////////////////////////////////
+//
+// Test naming convention
+//
+//    UnitOfWork_StateUnderTest_ExpectedOutcome
+//
+
+namespace ngPlay.back.WebAPI.Tests.Controllers
+{
+    [TestFixture]
+    public class EchoControllerTest
+    {
+        [Test]
+        public void HttpGet_BasicCall_ReturnsValueWithTimeAppended()
+        {
+            // ARRANGE
+            var controller = new EchoController();
+            const string value = "hello";
+
+            HttpContext.Current = new HttpContext(new HttpRequest("", "http://tempuri.org", ""),
+                                                  new HttpResponse(new StringWriter()));
+
+            // ACT
+            var echoResponse = controller.Get(value);
+
+            // ASSERT
+            Assert.NotNull(echoResponse);
+
+            var prop = echoResponse.GetType().GetProperty("echo");
+            Assert.NotNull(prop);
+
+            var echoValue = prop.GetValue(echoResponse) as string;
+            Assert.NotNull(echoValue);
+            Assert.IsTrue(echoValue.StartsWith(value));
+            Assert.IsTrue(echoValue.Length > value.Length);
+        }
+    }
+}
