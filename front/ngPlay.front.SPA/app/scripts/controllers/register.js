613d511df12e88abceddfbca7660d255d56d78db
Luke <luke.oreilly@gmail.com>
2014-04-21 20:50:43 +1000

    Lock Register form while working

--- front/ngPlay.front.SPA/app/scripts/controllers/register.js
+++ front/ngPlay.front.SPA/app/scripts/controllers/register.js
@@ -11,21 +11,26 @@ angular.module('ngPlay')
       confirmPassword : ''
     };
 
+    $scope.registerVm.working = false;
+
     $scope.registerVm.isRegistered = false;
 
     $scope.registerVm.errorOccurred = false;
     $scope.registerVm.errorInfo = '';
 
     $scope.registerVm.registerUser = function() {
-        UserAccountService.registerUser($scope.registerVm.userData).then(
-          function (data) {
-            $scope.registerVm.isRegistered = true;
-            $scope.registerVm.errorOccurred = false;
+      $scope.registerVm.working = true;
 
-          }, function (data) {
-            $scope.registerVm.errorOccurred = true;
-            $scope.registerVm.errorInfo = data;
+      UserAccountService.registerUser($scope.registerVm.userData).then(
+        function (data) {
+          $scope.registerVm.isRegistered = true;
+          $scope.registerVm.errorOccurred = false;
+          $scope.registerVm.working = false;
 
-          });
+        }, function (data) {
+          $scope.registerVm.errorOccurred = true;
+          $scope.registerVm.errorInfo = data;
+          $scope.registerVm.working = false;
+        });
     };
   });
